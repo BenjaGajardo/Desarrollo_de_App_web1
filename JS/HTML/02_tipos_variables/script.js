b03e9c3f4f0c25e4b2b64cfc21cea1e6f4b8304c
Benjamin Gajardo <Benjamingajardo@liceovvh.cl>
2025-03-10 11:16:35 -0300

    OK

---
+++ JS/HTML/02_tipos_variables/script.js
@@ -0,0 +1,14 @@
+//creacion de variables 
+const rut = 224443366
+let numero = 0; //numerico
+let decimal = 2.5; //num float
+let texto = "Hola"; //string
+let booleano = true; // bool
+
+let array = [1, "hola", 25]; //lista elementos
+let matriz = [[1,2],[3,4]]; //lista dentro de lista
+let objeto = {nombre: "Benja", Edad:18}; //objeto
+
+function sumar(){ //funcion suma
+    return a + b
+}
