ef1c5274ffc4261715ffb530e855fb87c0ce9b81
Benjamin Gajardo <Benjamingajardo@liceovvh.cl>
2025-03-11 10:28:53 -0300

    ok

--- JS/HTML/03_ejercicio_variables/script.js
+++ JS/HTML/03_ejercicio_variables/script.js
@@ -4,21 +4,45 @@ let numero = 0; //numerico
 let decimal = 2.5; //num float
 let texto = "Hola"; //string
 let booleano = true; // bool
+let indefinido = undefined;
+let nulo = null;
 
 let array = [1, "hola", 25]; //lista elementos
 let matriz = [[1,2],[3,4]]; //lista dentro de lista
-let objeto = {nombre: "Benja", Edad:18}; //objeto
-
-function sumar(){ //funcion suma
-    return a + b
-}
+let objeto = {nombre: "Benja", Edad:17}; //objeto
 
 
 function float(){
     alert(decimal);
 }
 
+function string(){
+    alert(texto)
+}
 
+function bool(){
+    alert(booleano)
+}
+
+function float_num(){
+    alert(numero)
+}
+
+function elements(){
+    alert(array)
+}
+
+function list(){
+    alert(matriz)
+}
+
+function object(){
+    alert(`Nombre: ${objeto.nombre}, Edad: ${objeto.Edad}`)
+}
+
+function M_nulo(){
+    alert(nulo)
+}
 
 
 
