8ad5b519b3b51a515e8d73a5f3f6558c6a032b6f
Benjamin Gajardo <benjamingajardo@liceovvh.cl>
2025-03-17 18:51:41 -0300

    ok

--- JS/HTML/05_Tarea_array_1.3/script.js
+++ JS/HTML/05_Tarea_array_1.3/script.js
@@ -1,9 +1,11 @@
+//Ejercicio 1 suma de indices
 function Suma_indices(){
     let array = [1,2,3,4,5,6,7];
     let ejercicio = array[6] + array[5] + array[0];
     alert("El resultado de la suma: " + ejercicio);
 }
 
+//Ejercicio 2 multiplicación de pares
 function Producto_pares(){
     let array = [2,4,6,8,10,12];
     let multiplicaccion = array[0] * array[1] * array[2] * array[3] * array[4] * array[5];
