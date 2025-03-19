15ed2fa0855eeb9a1205138c6d236511c82613e3
Benjamin Gajardo <benjamingajardo@liceovvh.cl>
2025-03-19 17:44:16 -0300

    ok

---
+++ JS/HTML/06_Tarea_Condicionales_1.4/script.js
@@ -0,0 +1,42 @@
+function _Temperatura(){
+    let temperatura = prompt("Ingrese la temperatura: ")
+    if (temperatura <= 0) {
+        alert("Hace frío.");
+    } else if (temperatura >= 0 && temperatura <= 25){
+        alert("La temperatura es agradable.")
+    } else  {
+    alert("Hace calor.")
+}
+}
+
+function _Usuario(){
+    let nombreUsuario = prompt("Ingrese el usuario: ");
+    let contraseña = prompt("Ingrese la contraseña: ");
+    if (nombreUsuario === "usuario123" && contraseña === "secreto"){
+        alert("Acceso concedido")
+    } else {
+        alert("Acceso denegado")
+    }
+}
+
+function _Numero(){
+    let numero = parseInt(prompt("Ingrese un número: "))
+    if (numero <= -1){
+        alert("El número es negativo")
+    } else if (numero >= 1) {
+        alert("El número es positivo")
+    } else {
+        alert("El número es cero")
+    }
+}
+
+function _Puntuacion(){
+    let puntuacion = parseInt(prompt("Ingrese puntuación: "))
+    if (puntuacion >= 90){
+        alert("Excelente")
+    } else if (puntuacion >= 70){
+        alert("Buen trabajo")
+    } else {
+        alert("Necesitas mejorar")
+    }
+}
