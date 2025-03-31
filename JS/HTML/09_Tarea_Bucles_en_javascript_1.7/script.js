d4ba63edffdcac6996c719d144ee36614f0cbb96
Benjamin <benjamingajardo@liceovvh.cl>
2025-03-31 10:26:49 -0300

    ok

--- JS/HTML/09_Tarea_Bucles_en_javascript_1.7/script.js
+++ JS/HTML/09_Tarea_Bucles_en_javascript_1.7/script.js
@@ -1,16 +1,22 @@
-function _Contador(){
-    let contador = 1;
-    while (contador <= 5){
-        alert(contador);
-        contador++;
+function contarNumeros(){
+    let n = parseInt(prompt("Ingrese un numero entero: "))
+    let count = 1;
+    let array = [];
+    while (count <= n){
+        array.push(count);
+        count++//suma 1 a la variable count
     }
+    alert(`Contando: ${array.join(" - ")}`);
 }
 
-function _Numeros(){
-    let suma = 0
-    for (let i = 1; i<= 5; i++){
-        suma += i; 
-    } alert("Esta es la suma de los 5 digitos: " + suma)
+function _Numero(){
+    let contador = 10;
+    let array = []; // crea una lista vacia 
+    while (contador >= 1){
+        array.push(contador); //inserta datos en este caso la lista(array)
+        contador--;
+    }
+    alert(`Los numeros son: ${array.join(" - ")}`); // join es para colocar entre la lista en este caso " - "
 }
 
 function _Multiplo(){
@@ -74,4 +80,19 @@ function _Numero(){
         contador--;
     }
     alert(`Los numeros son: ${array.join(" - ")}`); // join es para colocar entre la lista en este caso " - "
+}
+
+function _Numeros(){
+    let suma = 0
+    for (let i = 1; i<= 5; i++){
+        suma += i; 
+    } alert("Esta es la suma de los 5 digitos: " + suma)
+}
+
+function _Contador(){
+    let contador = 1;
+    while (contador <= 5){
+        alert(contador);
+        contador++;
+    }
 }
