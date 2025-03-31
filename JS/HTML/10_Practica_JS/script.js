add97c25f3f30ba0ef003556b42eb67d26c04fd7
Benjamin <benjamingajardo@liceovvh.cl>
2025-03-31 16:07:02 -0300

    ok

--- JS/HTML/10_Practica_JS/script.js
+++ JS/HTML/10_Practica_JS/script.js
@@ -2,6 +2,15 @@ function _multiplicar(){
     let n = parseInt(prompt("Ingresar un numero para  multiplicar: ")) 
     let array = []
     for (let i = 1; i<= 10; i++){
-        array.push(n * i);
-    }alert(`Resultados de multiplicación: ${array.join(" - ")}`)
+        array.push(`${n} * ${i} = ${n * i}`);
+    }alert(`Resultados de multiplicación:\n` + array.join("\n"))
+}
+
+function _suma(){
+    let n = parseInt(prompt("Cuantos numeros desea sumar: ")) 
+    let suma = 0; 
+    let numero = parseInt(prompt("Ingresa el numero: "))
+    for (let i = 1; i <= n; i++){
+        suma += numero;
+    }alert(`La suma es: ${suma} `)
 }
