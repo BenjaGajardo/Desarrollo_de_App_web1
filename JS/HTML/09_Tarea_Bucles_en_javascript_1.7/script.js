3d7e3bdf1694668ed586ff5a5bc9cc27fbf00ea5
Benjamin <benjamingajardo@liceovvh.cl>
2025-03-31 11:16:27 -0300

    ok

--- JS/HTML/09_Tarea_Bucles_en_javascript_1.7/script.js
+++ JS/HTML/09_Tarea_Bucles_en_javascript_1.7/script.js
@@ -9,21 +9,34 @@ function contarNumeros(){
     alert(`Contando: ${array.join(" - ")}`);
 }
 
-function _Numero(){
-    let contador = 10;
-    let array = []; // crea una lista vacia 
-    while (contador >= 1){
-        array.push(contador); //inserta datos en este caso la lista(array)
-        contador--;
-    }
-    alert(`Los numeros son: ${array.join(" - ")}`); // join es para colocar entre la lista en este caso " - "
+function _numero(){
+    let n = parseInt(prompt("Ingrese un numero: "));
+    let contador = 1;
+    let array = []
+    while (n >= contador) {
+        array.push(n); // Muestra el número actual
+        n--; // Decrementa el valor de n en 1
+    }alert(`Los numeros son: ${array.join(" - ")}`)
 }
 
-function _Numeros(){
-    let suma = 0
-    for (let i = 1; i<= 5; i++){
-        suma += i; 
-    } alert("Esta es la suma de los 5 digitos: " + suma)
+function _sumaNumeros(){
+    let sumar = 0;
+    let cantidad = parseInt(prompt("Ingresa la cantidad de números a sumar:"));
+    
+    for(let i = 1; i <= cantidad; i++){
+        sumar += i;
+    }
+    alert("Suma de los" + " primeros números: " + sumar);
+}
+
+function _multiplos(){
+    let limite = parseInt(prompt("Ingresa el límite de números a encontrar:"));
+    let array = []
+
+    for (let i = 2; i <= limite; i += 2) {
+        array.push(i);
+    }
+    alert(`Los multiplos son: ${array.join(" - ")}`)
 }
 
 function _Multiplo(){
@@ -62,4 +75,20 @@ function bucleFor(){
     }
     alert(`Los numeros son: ${array.join(" - ")}`)
 }
+//ejemplo
+function _Numero(){
+    let contador = 10;
+    let array = []; // crea una lista vacia 
+    while (contador >= 1){
+        array.push(contador); //inserta datos en este caso la lista(array)
+        contador--;
+    }
+    alert(`Los numeros son: ${array.join(" - ")}`); // join es para colocar entre la lista en este caso " - "
+}
 
+function _Numeros(){
+    let suma = 0
+    for (let i = 1; i<= 5; i++){
+        suma += i; 
+    } alert("Esta es la suma de los 5 digitos: " + suma)
+}
