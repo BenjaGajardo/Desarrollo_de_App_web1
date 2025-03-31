f2f1d1ee30b901f5f4b04a7855af8cf550b2aee1
Benjamin <benjamingajardo@liceovvh.cl>
2025-03-31 10:54:23 -0300

    ok

--- JS/HTML/09_Tarea_Bucles_en_javascript_1.7/script.js
+++ JS/HTML/09_Tarea_Bucles_en_javascript_1.7/script.js
@@ -3,75 +3,12 @@ function contarNumeros(){
     let count = 1;
     let array = [];
     while (count <= n){
-        array.push(count);
-        count++//suma 1 a la variable count
+        array.push(count);//Insertar un valor al final del array
+        count++//incrementa 1 al contador
     }
     alert(`Contando: ${array.join(" - ")}`);
 }
 
-function _Numero(){
-    let contador = 10;
-    let array = []; // crea una lista vacia 
-    while (contador >= 1){
-        array.push(contador); //inserta datos en este caso la lista(array)
-        contador--;
-    }
-    alert(`Los numeros son: ${array.join(" - ")}`); // join es para colocar entre la lista en este caso " - "
-}
-
-function _Multiplo(){
-    for (let i = 2; i<= 10; i += 2){
-        alert(i)
-    }
-}
-
-
-
-
-
-
-
-//Ejercicio de ejemplo 4
-function _NumerosS(){
-    let suma = 0
-    let array = []
-    for (let i = 1; i<= 5; i++){
-        suma += i; 
-        array.push 
-    } alert(`Contando: ${array.join(" - ")}`);
-}
-
-//Ejercicio hecho
-function _ContadoRR(){
-    let contador = 10;
-    while (contador >= 1){
-        alert(contador);
-        contador--;
-    }
-}
-
-//Ejemplo de ejercicio 3
-function contarNumeros(){
-    let n = parseInt(prompt("Ingrese un numero entero: "))
-    let count = 1;
-    let array = [];
-    while (count <= n){
-        array.push(count);
-        count++
-    }
-    alert(`Contando: ${array.join(" - ")}`);
-}
-
-//Ejemplo de ejercicio 2 
-function bucleFor(){
-    let array = [];
-    for (i = 10; i >= 1; i-- ){
-        array.push(i)
-    }
-    alert(`Los numeros son: ${array.join(" - ")}`)
-}
-
-//Ejercicio de ejemplo
 function _Numero(){
     let contador = 10;
     let array = []; // crea una lista vacia 
@@ -89,10 +26,47 @@ function _Numeros(){
     } alert("Esta es la suma de los 5 digitos: " + suma)
 }
 
+function _Multiplo(){
+    for (let i = 2; i<= 10; i += 2){
+        alert(i)
+    }
+}
+
+
+
+
+
+//Ejemplo 
 function _Contador(){
     let contador = 1;
     while (contador <= 5){
         alert(contador);
         contador++;
     }
+}
+
+//Ejercicio hecho
+function _ContadoRR(){
+    let contador = 10;
+    while (contador >= 1){
+        alert(contador);
+        contador--;
+    }
+}
+
+//Ejemplo de ejercicio 2 
+function bucleFor(){
+    let array = [];
+    for (i = 10; i >= 1; i-- ){
+        array.push(i)
+    }
+    alert(`Los numeros son: ${array.join(" - ")}`)
+}
+
+//Ejemplo
+function _Numeros(){
+    let suma = 0
+    for (let i = 1; i<= 5; i++){
+        suma += i; 
+    } alert("Esta es la suma de los 5 digitos: " + suma)
 }
